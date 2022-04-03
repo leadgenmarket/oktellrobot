f8a99dbfd1ecde7d5b5a192e7514d2dda5be22f6
kanya384 <kanya384@mail.ru>
2022-04-03 16:29:18 +0300

    proper php container, nodejs Dockerfile change

--- php/amocrm/index.php
+++ php/amocrm/index.php
@@ -9,8 +9,14 @@ $clientId = $_ENV["AMO_ID"];
 $clientSecret = $_ENV["AMO_SECRET"];
 $redirectUri = $_ENV["AMO_REDIRECT_URI"];
 
-include "amo_client.php";
-$amoClient = new LeadgenAmoClient($baseDomain, $clientId, $clientSecret, $redirectUri);
+$client = new MongoDB\Client("mongodb://".$_ENV["APP_DSN"]);
+$collection = $client->leadgen->amoBuf;
+$result = $collection->find()->toArray();
+
+echo"<pre>";print_r($result);echo"</pre>";
+
+/*include "amo_client.php";
+$amoClient = new LeadgenAmoClient($baseDomain, $clientId, $clientSecret, $redirectUri);*/
 //$lead = $amoClient->getLeadById(29201257);
 //var_dump($amoClient->getLeadsPhoneNumber($lead));
-var_dump($amoClient->getLeadsOfPipelineInStatus(2010945, 29589345));
+//var_dump($amoClient->getLeadsOfPipelineInStatus(2010945, 29589345));
